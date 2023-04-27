77129936ea779f3937a00ca57c075b44d1f175b8
Venom812 <skorodeev@gmail.com>
2023-04-27 17:35:24 +0300

    test view

---
+++ datascraper/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # path('run_forecasts_scraper', views.run_forecasts_scraper, name='run_forecasts_scraper'),
+    # path('run_archive_scraper', views.run_archive_scraper, name='run_archive_scraper'),
+    path('', views.test, name='test'),
+]
