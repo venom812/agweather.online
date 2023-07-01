e5ac46918fc2fe187be5b06f23fcc896c2cfc79b
Venom812 <skorodeev@gmail.com>
2023-07-01 23:21:24 +0300

    010723 Selenium mod

--- datascraper/forecasts.py
+++ datascraper/forecasts.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium_stealth import stealth
+import random
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-from pathlib import Path
 import re
 
 
@@ -213,6 +215,7 @@ def get_soup(url):
     except Exception as _ex:
         print(_ex)
     finally:
+        print(url)
         driver.close()
         driver.quit()
 
@@ -223,21 +226,75 @@ def get_soup(url):
 def init_selenium_driver():
     """Selenium driver initialization"""
 
+    # create a new Service instance and specify path to Chromedriver executable
+    service = ChromeService(executable_path=ChromeDriverManager().install())
+
+    # create a ChromeOptions object
     options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--no-sandbox")
+    # run in headless mode
+    options.add_argument("--headless")
+    # disable the AutomationControlled feature of Blink rendering engine
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    # disable pop-up blocking
+    options.add_argument('--disable-popup-blocking')
+    # start the browser window in maximized mode
+    options.add_argument('--start-maximized')
+    # disable extensions
+    options.add_argument('--disable-extensions')
+    # disable sandbox mode
+    options.add_argument('--no-sandbox')
+    # disable shared memory usage
+    options.add_argument('--disable-dev-shm-usage')
 
-    driver_executable_path = str(
-        Path(__file__).resolve().parent) + "/chromedriver"
+    # Waits for page to be interactive
+    options.page_load_strategy = 'eager'
 
-    caps = DesiredCapabilities().CHROME
-    # caps["pageLoadStrategy"] = "normal"  #  Waits for full page load
-    caps["pageLoadStrategy"] = "eager"  # Waits for page to be interactive
-    # caps["pageLoadStrategy"] = "none"   # Do not wait for full page load
+    # create a driver instance
+    driver = webdriver.Chrome(service=service, options=options)
+    # Change the property value of the navigator for webdriver to undefined
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', \
+                          {get: () => undefined})")
 
-    return webdriver.Chrome(executable_path=driver_executable_path,
-                            options=options, desired_capabilities=caps)
+    user_agents = [
+        # Add your list of user agents here
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+            (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 \
+            (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+            (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 \
+            (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+            (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 \
+            (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 \
+            (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+    ]
+
+    # select random user agent
+    user_agent = random.choice(user_agents)
+
+    # pass in selected user agent as an argument
+    options.add_argument(f'user-agent={user_agent}')
+
+    # enable stealth mode
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
+
+    # caps = DesiredCapabilities().CHROME
+    # # caps["pageLoadStrategy"] = "normal"  #  Waits for full page load
+    # caps["pageLoadStrategy"] = "eager"  # Waits for page to be interactive
+    # # caps["pageLoadStrategy"] = "none"   # Do not wait for full page load
+
+    return driver
 
 
 def func_start_date_from_source(month, day, req_start_datetime):
